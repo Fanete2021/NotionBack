1e5058033738372e1427e281ed588f588e2d2e9f
Dmitriy <dmitrhut@mail.ru>
2026-07-14 21:49:27 +0300

    Настроен Prittier и Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,9 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
