73d4d5c0b614e58c7db2dfc3d7c665cf6d4289d1
Dmitriy <dmitrhut@mail.ru>
2026-07-17 21:43:41 +0300

    исправлены описания для свэггера

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Notion API')
+    .setDescription('Notion API')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Notion')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
