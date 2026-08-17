a1fcf5a18e718147bd8b4a881a3b192afa707169
dmitr <dmitrhut@mail.ru>
2026-08-17 10:15:09 +0300

    fix(AuthController): добавлены комменты к secure: true

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -58,7 +58,7 @@ export class AuthController {
       sameSite: 'none',
       maxAge: maxAgeSeconds * 1000, // Переводим секунды в миллисекунды для cookie
       path: '/',
-      secure: false,
+      secure: false, // На этапе разработки ставим false чтобы фронт мог работать с cookie, в продакшене нужно ставить true
     });
   }
 
@@ -67,7 +67,7 @@ export class AuthController {
       httpOnly: true,
       sameSite: 'none',
       path: '/',
-      secure: false,
+      secure: false, // На этапе разработки ставим false чтобы фронт мог работать с cookie, в продакшене нужно ставить true
     });
   }
 
