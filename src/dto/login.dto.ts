f7db9d7f735c4c78d628da7c3252c1e7f14fe037
dmitr <dmitrhut@mail.ru>
2026-09-02 23:16:09 +0300

    [NOT-19] fix(login dto): теперь при логине пароль не проверяется по регулярному выражению, чтобы убрать 422 ошибку

--- src/dto/login.dto.ts
+++ src/dto/login.dto.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer';
-import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
@@ -16,8 +16,5 @@ export class LoginDto {
   @ApiProperty({ example: 'password123', description: 'Пароль' })
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)/, {
-    message: 'Password must contain at least one letter and one number',
-  })
   password!: string;
 }
